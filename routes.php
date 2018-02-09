226073628e1702c2e9bc4f3bb1859267fe37457a
Robert Wittman <robkwittman@gmail.com>
2018-02-08 21:55:32 -0700

    Fix routes

--- routes.php
+++ routes.php
@@ -3,7 +3,8 @@
 use LaravelShopifyPlugin\Http\Controllers\Auth;
 use LaravelShopifyPlugin\Http\Controllers\Install;
 
-Route::get(['prefix' => 'shopify'], function() {
+Route::group(['prefix' => 'shopify'], function() {
     Route::match(['get', 'post'], '/install', Install::class);
     Route::post('uninstall', Uninstall::class);
+
 });
